baa409892b5439e73774ee554bafde7aa5e48c71
mukeshk2212 <mukeshcse37@gmail.com>
2026-07-06 11:00:52 +0530

    second commit

--- main.py
+++ main.py
@@ -145,3 +145,4 @@ async def http_exception_handler(request: Request, exc: HTTPException):
         status_code=exc.status_code,
         content={"status": "error", "message": exc.detail},
     )
+#
